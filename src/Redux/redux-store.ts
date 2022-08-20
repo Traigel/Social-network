2725e0e8dbf1390f7a891d5cb75debf1983cc506
Vladimir Traihel <vovatraigel@gmail.com>
2022-08-20 23:16:55 +0300

    75 - redux-form

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -5,6 +5,7 @@ import {sidebarReducer} from "./sidebar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import thunkMiddleware from 'redux-thunk'
+import {reducer as  formReducer} from "redux-form";
 
 
 const rootReducer = combineReducers ({
@@ -13,6 +14,7 @@ const rootReducer = combineReducers ({
     sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {reduxForm} from "redux-form";
+
+const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <Field placeholder={'Login'} component={'input'}/>
+            </div>
+            <div>
+                <Field placeholder={'Password'}/>
+            </div>
+            <div>
+                <Field type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm)
 
 export const Login = () => {
     return <div>
         <h1>Login</h1>
+        <LoginReduxForm/>
     </div>
 };
