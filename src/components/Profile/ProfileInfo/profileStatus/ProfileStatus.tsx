eb59693e23e4cbd25b58c86e907dd9041b86a464
Vladimir Traihel <vovatraigel@gmail.com>
2022-08-20 22:42:59 +0300

    74 - componentDidUpdate

--- src/components/Profile/ProfileInfo/profileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/profileStatus/ProfileStatus.tsx
@@ -13,6 +13,14 @@ export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
         status: this.props.status
     }
 
+    componentDidUpdate(prevProps: Readonly<ProfileStatusPropsType>, prevState: Readonly<{}>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     activateEditMode() {
         this.setState({
             editMode: true
