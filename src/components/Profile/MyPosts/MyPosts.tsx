343630a22a58f2453859f25bd23af3bdf1c10b85
Vladimir Traihel <vovatraigel@gmail.com>
2022-10-13 19:38:23 +0300

    87 - shouldComponentUpdate, PureComponent, memo

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,15 +4,22 @@ import {MyPostsPropsType} from "./MyPostsContainer";
 import {Post} from "./Post/Post";
 import {AddPostFormRedux, AddPostFormType} from "./addPostForm/AddPostForm";
 
-export const MyPosts = (props: MyPostsPropsType) => {
+export class MyPosts extends React.PureComponent<MyPostsPropsType> {
 
-    const addNewPost = (formData: AddPostFormType) => {
-        props.addPost(formData)
+    // shouldComponentUpdate(nextProps: Readonly<MyPostsPropsType>, nextState: Readonly<{}>, nextContext: any): boolean {
+    //     return nextProps !== this.props || nextState !== this.state
+    // }
+
+    render() {
+
+        const addNewPost = (formData: AddPostFormType) => {
+            this.props.addPost(formData)
+        }
+
+        return <div className={style.myPosts}>
+            <h3>My posts</h3>
+            <AddPostFormRedux onSubmit={addNewPost}/>
+            {this.props.profilePage.posts.map(u => <Post key={u.id} message={u.message} likes={u.likes} id={u.id}/>)}
+        </div>
     }
-
-    return <div className={style.myPosts}>
-        <h3>My posts</h3>
-        <AddPostFormRedux onSubmit={addNewPost}/>
-        {props.profilePage.posts.map(u => <Post key={u.id} message={u.message} likes={u.likes} id={u.id}/>)}
-    </div>
-};
+}
