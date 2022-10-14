432fdd33b176253398735d8115109b1b306a4278
Vladimir Traihel <vovatraigel@gmail.com>
2022-10-14 15:13:03 +0300

    91 - chrome extensions, react and redux

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,15 +1,14 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore, compose} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {sidebarReducer} from "./sidebar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import thunkMiddleware from 'redux-thunk'
-import {reducer as  formReducer} from "redux-form";
+import {reducer as formReducer} from "redux-form";
 import {AppReducer} from "./app-reducer";
 
-
-const rootReducer = combineReducers ({
+const rootReducer = combineReducers({
     app: AppReducer,
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
@@ -19,9 +18,9 @@ const rootReducer = combineReducers ({
     form: formReducer
 })
 
-export type AppStateType = ReturnType<typeof rootReducer>
-
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
-
 // @ts-ignore
-window.store = store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+export type AppStateType = ReturnType<typeof rootReducer>
